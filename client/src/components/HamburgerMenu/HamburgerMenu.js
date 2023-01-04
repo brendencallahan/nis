797e069f5c5fec08ee507b9c953e2fc25cc54bbb
Brenden Callahan <brendenkcallahan@gmail.com>
2023-01-03 18:45:23 -0800

    Starting search resulsts page

--- client/src/components/HamburgerMenu/HamburgerMenu.js
+++ client/src/components/HamburgerMenu/HamburgerMenu.js
@@ -1,5 +1,12 @@
 export default function HamburgerMenu() {
-  return ( //placeholder to test dark mode
-    <button onClick={() => {document.body.classList.toggle("dark")}}>Theme</button>
+  return (
+    //placeholder to test dark mode
+    <button
+      onClick={() => {
+        document.body.classList.toggle('dark');
+      }}
+    >
+      Theme
+    </button>
   );
 }
