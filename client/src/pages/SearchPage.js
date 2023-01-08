fb7b022cb084542998aaf36528a6e533474575c2
Brenden Callahan <brendenkcallahan@gmail.com>
2023-01-08 12:49:43 -0800

    Added infinite scroll. Need to update main page when reach the end of results

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -9,7 +9,7 @@ import Result from '../components/Result';
 export default function Apod() {
   const [results, setResults] = useState([]);
   const [culledResults, setCulledResults] = useState([]);
-  const [mainPage, setMainPage] = useState(1);
+  const [page, setPage] = useState(1);
   const [loaded, setLoaded] = useState(false);
   const lastPic = useRef(null);
   const query = useQuery();
@@ -23,8 +23,11 @@ export default function Apod() {
     const source = axios.CancelToken.source();
     const fetchData = async (params) => {
       try {
-        setResults(await getResults(query, 1)); //Second argument is page to get. Start with 1 on new queries
+        const data = await getResults(query, 1)
+        setResults(data.slice(25)); //Second argument is page to get. Start with 1 on new queries
+        setCulledResults(data.slice(0, 24));
         setLoaded(true);
+        setPage(1);
       } catch (err) {
         if (axios.isCancel(err)) {
           console.log(err.message);
@@ -43,6 +46,8 @@ export default function Apod() {
     const [entry] = entries;
     let isLeaving = false;
     if (entry.isIntersecting) {
+      setCulledResults((old) => [...old, ...cullResults(results, page)])
+      setPage((page) => page + 1);
       console.log(entry);
       isLeaving = true; // May need to remove. Pic jumping down might leave this as true
     } else if (isLeaving) {
@@ -51,7 +56,7 @@ export default function Apod() {
   };
 
   useEffect(() => {
-
+    if (loaded) {
       const options = { root: null, rootMargin: '200%', threshold: 0 };
       const observer = new IntersectionObserver(getMorePics, options);
       const disconnectPic = lastPic.current;
@@ -60,23 +65,24 @@ export default function Apod() {
       return () => {
         if (disconnectPic) observer.unobserve(disconnectPic);
       };
-  }, [lastPic]);
+    }
+  }, [lastPic, loaded]);
 
   return (
-    <div className="pt-10">
+    <div className='pt-10'>
       <SearchBar />
       {!loaded ? (
         <div className='h-screen'>Loading...</div>
       ) : (
         <>
           <div>
-            {results.map((result) => {
+            {culledResults.map((result) => {
               return <Result result={result} />;
             })}
+            <div ref={lastPic}></div>
           </div>
         </>
       )}
-      <div ref={lastPic}></div>
     </div>
   );
 }
