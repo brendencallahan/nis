de17fdc2361a4b6d7d2c7ed35bf422f347c8d87f
Brenden Callahan <brendenkcallahan@gmail.com>
2023-01-09 22:13:23 -0800

    Saved dark mode preference to localstorage

--- client/src/components/HamburgerMenu/HamburgerMenu.js
+++ client/src/components/HamburgerMenu/HamburgerMenu.js
@@ -1,12 +1,31 @@
+import { useState, useEffect } from "react";
+
 export default function HamburgerMenu() {
+
+  const [ darkMode, setDarkMode ] = useState(JSON.parse(localStorage.getItem("darkMode")))
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark')
+    } else if (!darkMode) {
+      document.body.classList.remove('dark')
+    }
+  },[darkMode])
+
+  const handleDarkMode = () => {
+    localStorage.setItem("darkMode", JSON.stringify(!darkMode))
+    setDarkMode((current) => !current)
+  };
+
   return (
     //placeholder to test dark mode
-    <button className=""
-      onClick={() => {
-        document.body.classList.toggle('dark');
-      }}
-    >
-      <svg className="fill-base-dark dark:fill-gray-light" viewBox="0 0 100 80" width="25" height="25">
+    <button className="" onClick={handleDarkMode}>
+      <svg
+        className="fill-base-dark dark:fill-gray-light"
+        viewBox="0 0 100 80"
+        width="25"
+        height="25"
+      >
         <rect width="100" height="12.5" rx="10"></rect>
         <rect y="30" width="100" height="12.5" rx="10"></rect>
         <rect y="60" width="100" height="12.5" rx="10"></rect>
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo, useRef, useCallback } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import cullResults from '../utils/cullResults';
