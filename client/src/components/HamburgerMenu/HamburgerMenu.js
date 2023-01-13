3e1528afcdf8c3bc30a950d1cd8b6aec0b952bcd
Brenden Callahan <brendenkcallahan@gmail.com>
2023-01-13 07:23:04 -0800

    Working version of clickoutside closing the modal

--- client/src/components/HamburgerMenu/HamburgerMenu.js
+++ client/src/components/HamburgerMenu/HamburgerMenu.js
@@ -1,26 +1,46 @@
-import { useState } from 'react';
+import { useRef, useState} from 'react';
+import  { useClickAway } from 'react-use'
 import HamburgerModal from './HamburgerModal';
-import useBlockClicks from '../../utils/useBlockClicks';
+
 
 export default function HamburgerMenu() {
-  const [ ref, shouldBlock ] = useBlockClicks(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null);
+  const buttonRef = useRef(null);
+
+  function handleOpening() {
+    setIsOpen(current => !current)
+  }
+
+  useClickAway(ref, (event) => {
+    if (buttonRef.current.contains(event.target)) {
+      handleOpening();
+    } else {
+      setIsOpen(false);
+    }
+
+
+    console.log("working");
+  })
+
 
   return (
     <>
-        <div className="" ref={ref}>
-      <button className="relative" >
+      <button ref={buttonRef}>
         <svg
           className="fill-base-dark dark:fill-gray-light"
           viewBox="0 0 100 80"
           width="25"
           height="25"
-        >
+          >
           <rect width="100" height="12.5" rx="10"></rect>
           <rect y="30" width="100" height="12.5" rx="10"></rect>
           <rect y="60" width="100" height="12.5" rx="10"></rect>
         </svg>
       </button>
-        {shouldBlock  &&<HamburgerModal />}
+      <div className='absolute' ref={ref}>
+
+        {isOpen &&<HamburgerModal />}
       </div>
     </>
   );
