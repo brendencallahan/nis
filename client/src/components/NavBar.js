26581efe7c2ad248bae4d5631d5e8d45e3363f93
Brenden Callahan <brendenkcallahan@gmail.com>
2023-01-13 10:31:36 -0800

    Fixed top navbar spacing

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,15 +4,18 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
+    <div className='sticky top-0 shadow-md z-10'>
+
     <div
       className={`
-      sticky top-0 z-10 mb-10 flex justify-between bg-gray-light dark:bg-base-dark py-5 px-[2.5vw] md:px-[15vw] lg:px-[30vw] shadow-md`}
-    >
+      mb-10 flex justify-between bg-gray-light dark:bg-base-dark py-5 max-w-[95vw] ml-[2.5vw] md:max-w-[70vw] md:ml-[15vw] lg:max-w-[40vw] lg:ml-[30vw]`}
+      >
       <Link to="/" className="text-xl nis-font pt-[8.5px]">
         NIS
       </Link>
       <ScrollTop />
       <HamburgerMenu />
     </div>
+      </div>
   );
 }
