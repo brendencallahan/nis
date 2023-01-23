9c229e124cb6f5a7af57a3436f9406ea9da1dc7f
Brenden Callahan <brendenkcallahan@gmail.com>
2023-01-23 11:35:04 -0800

    Update aboutpage, contactpage, and homepage to make NIS's purpose clearer

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,27 @@
-import Apod from '../components/Apod';
-import SearchBar from '../components/SearchBar/SearchBar';
+import Apod from "../components/Apod";
+import SearchBar from "../components/SearchBar/SearchBar";
 
 export default function HomePage() {
   return (
     <>
-      <h1 className="text-transparent bg-clip-text bg-gradient-to-t from-slate-700 to-base-dark dark:from-gray-light dark:to-slate-300 nis-font text-center text-7xl mb-10 ml-2.5">NIS</h1>
+      <h1
+        className="text-transparent
+        bg-clip-text bg-gradient-to-t from-slate-700 to-base-dark
+        dark:from-gray-light dark:to-slate-300 
+        nis-font text-center text-7xl mb-5 ml-2.5"
+      >
+        NIS
+      </h1>
+      <h2
+        className="text-transparent
+        bg-clip-text bg-gradient-to-t from-slate-700 to-base-dark
+        dark:from-gray-light dark:to-slate-300 
+        nis-font text-center text-2xl mb-14 ml-2.5"
+      >
+        Nasa Image Search
+      </h2>
       {/* Add NavMenu */}
-      <SearchBar/>
+      <SearchBar />
       <Apod />
     </>
   );
